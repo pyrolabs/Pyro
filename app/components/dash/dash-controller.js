0d3305d83a1a2a67df3797bc3d2aee08085236f9
Scott Prue <prescottprue@gmail.com>
2014-11-30 04:44:45 -0500

    DatabaseUrl added to admin pannel. PyroLibrary now loading within dash controller.

--- app/components/dash/dash-controller.js
+++ app/components/dash/dash-controller.js
@@ -11,17 +11,17 @@ angular.module('pyroApp.controllers')
     pyroMaster.library.loadObject('instances', instance.name, function(loadedInstance){
       console.log('pyroInstance loaded');
       $scope.pyroInstance = loadedInstance;
-      // $scope.pyroInstance = pyroMaker(instance.dbUrl);
     })
     
   }
-  // console.log('scope set:', $scope.pyroInstance);
-  // $scope.userCount = $scope.pyroInstance.getObjectCount('users').then(function(count){
-  // 	$scope.userCount = count;
-  // });
-  // $scope.pyroInstance.getObjectCount('sessions').then(function(sessionCount){
-  // 	$scope.sessionCount = sessionCount;
-  // });
+  var instancePyro = pyroMaker(instance.dbUrl);
+  console.log('scope set:', $scope.pyroInstance);
+  $scope.userCount = instancePyro.getObjectCount('users').then(function(count){
+  	$scope.userCount = count;
+  });
+  instancePyro.getObjectCount('sessions').then(function(sessionCount){
+  	$scope.sessionCount = sessionCount;
+  });
 
   
 })
