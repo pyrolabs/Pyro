9873dd11288fc1aa0942b72882cf5a48a35833b7
Sinan Bolel <dev@sinanbolel.com>
2014-12-01 08:56:00 -0500

    Made the styles of the left menu of the dash a lot better.

--- app/components/tester/tester-controller.js
+++ app/components/tester/tester-controller.js
@@ -1,8 +1,16 @@
 angular.module('pyroApp.controllers')
 
-.controller('TesterCtrl', function($scope, $state, $rootScope, $stateParams, $sce) {
+.controller('TesterCtrl', function($scope, $state, $rootScope, $stateParams, $sce, $document) {
   console.log('TesterCtrl');
  
+  $scope.emuRender = {
+    url:'',
+    width:'100%',
+    height:'100%',
+    topmargin:'0',
+    type:'web'
+  } 
+
   $scope.otherDash = function(ind){
     $state.go('dash',{appId:ind})
   }
@@ -45,13 +53,41 @@ angular.module('pyroApp.controllers')
 
   $scope.getS3BucketContents = function(bucketName) {}
 
+  $scope.setEmulator = function(emu) {
+    $scope.emuRender.type = emu;
+    if (emu==='iphone6') {
+      console.log('Setting emulation mode to iphone6');
+      setEmulatorSize('375','667','px');
+    }
+    else if (emu==='iphone5') {
+      console.log('Setting emulation mode to iphone5');
+      setEmulatorSize('320','568','px');
+    }
+    else {
+      console.log('Setting emulation mode to web');
+      setEmulatorSize('100','100','%');
+    }
+  }
+
+  function setEmulatorSize(x,y,t) {
+    // var emu = $document.getElementsByClassName("tester-container"));
+    // console.log(emu);
+    console.log(x+t,y+t)
+    if(t=='px') {
+      $scope.emuRender.width = x;
+      $scope.emuRender.height = y;
+    } else {
+      $scope.emuRender.width = x+t;
+      $scope.emuRender.height = y+t;
+    }
+  }
 
    $scope.instanceList.$loaded().then(function(pyroList){
       // [TODO] get pyro object by selecting from exisiting list
     $scope.isLoading = false;
     console.log('scope set:', $scope.instanceList[0]);
     $scope.pyroInstance = pyroList[$stateParams.appId]
-    $scope.currentBucketUrl = $sce.trustAsResourceUrl('http://'+$scope.pyroInstance.appUrl);
+    $scope.emuRender.url = $sce.trustAsResourceUrl('http://'+$scope.pyroInstance.appUrl);
     $scope.pyroInstance.getUserCount(function(userCount){
       $scope.userCount = userCount;
       if(!$scope.$$phase) {
