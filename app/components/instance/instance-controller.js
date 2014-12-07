2a07971ad0e1716cf46472a72bacc8ee46eeadf3
Sinan Bolel <dev@sinanbolel.com>
2014-12-07 15:46:35 -0500

    Added code to instance-controller to make the user and session metrics for each added app work - but shabby. [Delivers #84078710]

--- app/components/instance/instance-controller.js
+++ app/components/instance/instance-controller.js
@@ -20,11 +20,52 @@ angular.module('pyroApp.controllers')
   $scope.instancesArray.$loaded().then(function(loadedList){
     $rootScope.instanceList = loadedList;
     console.log('$rootScope.instanceList set:', $scope.instanceList);
+
+    angular.forEach($scope.instancesArray, function(value, key) {
+      populateInstanceUserCounts(value, key);
+    });
   });
+ 
   $scope.instancesArray.$watch(function(event){
 
   });
-  // [TODO] Use different objects for the different create types
+
+  function populateInstanceUserCounts(argValue, argKey) {
+    var ref = new Firebase(argValue.dbUrl);
+    // Attach an asynchronous callback to read the data at our posts reference
+    ref.once("value", function(snapshot) {
+      var db = snapshot.val();
+      if(db.users) {
+        var count = countThis(db.users);
+        console.log("length of users ",count);
+        $scope.instancesArray[argKey].userCount = count;
+      } else {
+        console.log("No users found.");
+        $scope.instancesArray[argKey].userCount = 0;
+      }
+      if(db.sessions){
+        var count2 = countThis(db.sessions);
+        console.log("Total Sessions: ",count2);
+        $scope.instancesArray[argKey].sessionCount = count2;
+
+      } else {
+        console.log("No sessions found.");
+        $scope.instancesArray[argKey].sessionCount = 0;
+      }
+       if(!$scope.$$phase) {
+          $scope.$apply();
+        }
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+        return null
+    });
+  }
+
+  function countThis(obj) {
+    return _.countBy(obj, function() {
+      return 'count';
+    }).count;
+  }
   $scope.viewDetail = function(argName) {
     console.log('viewDetail called with: ', argName);
     console.log('loading:', $rootScope.instanceList.$getRecord(argName));
