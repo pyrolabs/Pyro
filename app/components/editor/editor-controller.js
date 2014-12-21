1e191672b8960a230c9fb0b59b3cfd157cf6a74b
Sinan Bolel <dev@sinanbolel.com>
2014-12-21 02:01:10 -0500

    Made the Upload button in Editor very visible.

--- app/components/editor/editor-controller.js
+++ app/components/editor/editor-controller.js
@@ -20,6 +20,7 @@ angular.module('pyroApp.controllers')
   // EDITOR
   $scope.dir = {};
   $scope.loading.editor = true;
+  // $scope.setFolderTreeState(true);
   $scope.aceLoaded = function(_editor){
     console.log('[EditorCtrl] Ace editor loaded successfully');
     $scope.editorObj = _editor;
@@ -64,7 +65,10 @@ angular.module('pyroApp.controllers')
       $scope.err = err;
     })
   };
-  $scope.collapseFolders = function() {
+  $scope.setFolderTreeState = function(state) {
+    $scope.files.$collapsed = state;
+  };
+  $scope.toggleFolders = function() {
     $scope.files.$collapsed = !($scope.files.$collapsed);
   };
   $scope.openFile = function(fileObject){
