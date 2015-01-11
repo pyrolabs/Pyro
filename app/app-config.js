8cb62f253ee0bee4968babe930c7174d5ce1e096
Scott Prue <prescottprue@gmail.com>
2015-01-11 04:57:36 -0500

    Build task run with newly working features. Disabled dev conviences (trackjs and config).

--- app/app-config.js
+++ app/app-config.js
@@ -2,6 +2,6 @@ angular.module('pyroApp.config', [])
 
 .constant('version', '1.0.0-a.2')
 
-.constant('SERVERURL', 'http://localhost:4000/dev/')
+.constant('SERVERURL', 'https://pyro-server.herokuapp.com/staging/')
 
 ;
