f62e8431af9c5e881ebdc32a78964ef56350bdee
Scott Prue <prescottprue@gmail.com>
2015-01-13 17:00:07 -0500

    [RELEASE] Release v1.0.0-a.3

--- app/app-config.js
+++ app/app-config.js
@@ -1,7 +1,7 @@
 angular.module('pyroApp.config', [])
 
-.constant('version', '1.0.0-a.2')
+.constant('version', '1.0.0-a.3')
 
-.constant('SERVERURL', 'https://pyro-server.herokuapp.com/1.0.0-a.2/')
+.constant('SERVERURL', 'https://pyro-server.herokuapp.com/1.0.0-a.3/')
 
 ;
